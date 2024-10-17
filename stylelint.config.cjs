eacf819c1e579e3eef5825668bc5949c429e5358
王德康 <3177146160@qq.com>
2024-10-17 18:27:47 +0800

    test

---
+++ stylelint.config.cjs
@@ -0,0 +1,10 @@
+// stylelint.config.cjs
+module.exports = {
+  extends: ['stylelint-config-standard'],
+  customSyntax: 'postcss-less',
+  plugin: ['stylelint-less'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'selector-class-pattern': null,
+  },
+};
